395a3d2378c42289a7fea7f7404b832a33088987
Lucas Módolo <lucasmg2001@gmail.com>
2020-09-11 17:01:48 -0300

    botão pagina de pagamento e botão de logout

--- js/pagamento.js
+++ js/pagamento.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var images = ['img/csgo.jpg', 'img/fortnite.jpg', 'img/freefire.jpg', 'img/fifa.jpg', 'img/lol.jpg', 'img/fifamobile.jpg'];
+    var images = ['img/csgo.jpg', 'img/fortnite.jpg', 'img/freefire.jpg', 'img/fifa.jpg', 'img/lol.jpg', 'img/fifamobile.jpg']
 
     $('body').append('<style>body{background: url(' + images[Math.floor(Math.random() * images.length)] + ') no-repeat center / cover}')
 
