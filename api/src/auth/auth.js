e37607e0cf23335cf663ed2155a295e2da0c88c2
Lucas Módolo <lucasmg2001@gmail.com>
2020-10-25 23:09:32 -0300

    construção do template radical teen v2

--- api/src/auth/auth.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt')
-
-exports.generateHash = (password) => {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-}
-
-exports.validPassword = (password, hash) => {
-    return bcrypt.compareSync(password, hash)
-}
